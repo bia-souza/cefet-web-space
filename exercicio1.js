36d06dd888deea38a8275a7d7b06c19da3ef8ecf
Beatriz Souza da Silva <beatriz.souza.silva@accenture.com>
2021-06-22 16:29:30 -0300

    Exercicio 1 - calculando força gravitacional

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,20 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+
+let botaoCalcular = document.querySelector('#calcular');
+	
+let calculaGravidade = () => {
+	
+	const G = document.querySelector('#constante').value;
+	const m1 = document.querySelector('#massa1').value;
+	const m2 = document.querySelector('#massa2').value;
+	const d = document.querySelector('#distancia').value;
+
+    const F = G*m1*m2/(d*d);
+
+    document.querySelector('#resultado').value = F;
+
+} 
+
+botaoCalcular.addEventListener('click', calculaGravidade); 
