04f74c7265cb1d2d178250d20a47f1931197ecc2
Beatriz Souza da Silva <beatriz.souza.silva@accenture.com>
2021-06-22 16:47:27 -0300

    Exercicio 3 - botoes expansiveis

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,21 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const p = document.querySelectorAll('.botao-expandir-retrair');
+
+const expandirTexto = (event) => {
+	
+    const button1 = event.currentTarget;
+    const paragrafo = button1.parentNode;
+    expandiu = paragrafo.classList.toggle('expandido');
+	
+	if(expandiu) {
+        button1.innerHTML = '-';
+    } else {
+        button1.innerHTML = '+';
+    }
+}
+
+p.forEach(p => {
+    p.addEventListener('click', expandirTexto)
+}); 
